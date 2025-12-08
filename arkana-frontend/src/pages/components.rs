4607b90b089a0cef559983639f99457ac9b9017d
Nícolas Ávila <239833085+avilaops@users.noreply.github.com>
2025-12-08 17:48:21 +0000

    Delete arkana-frontend directory

--- arkana-frontend/src/pages/components.rs
+++
@@ -1,5 +0,0 @@
-mod footer;
-mod header;
-
-pub use footer::Footer;
-pub use header::Header;
--- arkana-frontend/src/pages/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod components;
-
-mod home;
-pub use home::Home;
-
-// TODO: Implementar outras páginas
-// mod products;
-// mod cart;
-// mod checkout;
